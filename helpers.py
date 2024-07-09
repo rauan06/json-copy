cf00930b0163f71bed393e56edb5b3b9e701b696
rauan <ommirkulr@gmail.com>
2024-07-09 14:45:14 +0500

    created C code

--- helpers.py
+++ helpers.py
@@ -1,2 +1,21 @@
-def encode_basestring():
-    pass
+import ctypes
+import subprocess
+
+run_command = ['./hello']
+run_result = subprocess.run(run_command, capture_output=True, text=True)
+    
+# Print the output of the C program
+print("Program output: " + run_result.stdout)
+
+
+# Load the shared library
+# Use 'mylib.dll' on Windows
+mylib = ctypes.CDLL('./mylib.so')
+
+# Define the argument and return types of the add function
+mylib.add.argtypes = [ctypes.c_int, ctypes.c_int]
+mylib.add.restype = ctypes.c_int
+
+# Call the add function
+result = mylib.add(1000000, 3)
+print(f"Result of add(1000000, 3): {result}")
---
+++ mylib.c
@@ -0,0 +1,6 @@
+// mylib.c
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
